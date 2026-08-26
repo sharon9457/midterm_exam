fcd88a93e240b81a50626a05e2c2629031af9eb9
sharon9457 <sharon.cho.tw@gmail.com>
2026-08-26 10:27:50 +0800

    Update Q01_InventoryItem.java

---
+++ Q01_InventoryItem.java
@@ -0,0 +1,16 @@
+
+public class Q01_InventoryItem {
+private final String id;
+private final String name;
+private int stock;
+public Q01_InventoryItem(String id, String name, int stock){
+
+}
+public String getId(){}
+ 
+public String getName()
+public int getStock()
+public boolean restock(int amount)
+public boolean sell(int amount)
+public String status()
+}
